b33cf1c0ef12ad3a0c31df3c97c208b0619b4bce
ChinoZanotti <zanotti@arsis.com.py>
2025-07-05 11:07:47 -0300

    limpiando el codigo con loops para los nombres

--- js/script.js
+++ js/script.js
@@ -1,69 +1,17 @@
-let deck;
+let deck = []; //se crea una variable "deck" vacía
+let palos = ["a", "b", "c", "d"]; //se definen los palos o grupos que hay dentro del deck
+let drawBtn = document.getElementById("drawBtn");
+let shuffleResetBtn = document.getElementById("shuffleResetBtn");
 
-function reset () {
-	deck = [
-		"images/card-a-01.jpg",
-		"images/card-a-02.jpg",
-		"images/card-a-03.jpg",
-		"images/card-a-04.jpg",
-		"images/card-a-05.jpg",
-		"images/card-a-05.jpg",
-		"images/card-a-06.jpg",
-		"images/card-a-07.jpg",
-		"images/card-a-08.jpg",
-		"images/card-a-09.jpg",
-		"images/card-a-10.jpg",
-		"images/card-a-11.jpg",
-		"images/card-a-12.jpg",
-		"images/card-a-13.jpg",
-		"images/card-a-14.jpg",
-		"images/card-b-01.jpg",
-		"images/card-b-02.jpg",
-		"images/card-b-03.jpg",
-		"images/card-b-04.jpg",
-		"images/card-b-05.jpg",
-		"images/card-b-05.jpg",
-		"images/card-b-06.jpg",
-		"images/card-b-07.jpg",
-		"images/card-b-08.jpg",
-		"images/card-b-09.jpg",
-		"images/card-b-10.jpg",
-		"images/card-b-11.jpg",
-		"images/card-b-12.jpg",
-		"images/card-b-13.jpg",
-		"images/card-b-14.jpg",
-		"images/card-c-01.jpg",
-		"images/card-c-02.jpg",
-		"images/card-c-03.jpg",
-		"images/card-c-04.jpg",
-		"images/card-c-05.jpg",
-		"images/card-c-05.jpg",
-		"images/card-c-06.jpg",
-		"images/card-c-07.jpg",
-		"images/card-c-08.jpg",
-		"images/card-c-09.jpg",
-		"images/card-c-10.jpg",
-		"images/card-c-11.jpg",
-		"images/card-c-12.jpg",
-		"images/card-c-13.jpg",
-		"images/card-c-14.jpg",
-		"images/card-d-01.jpg",
-		"images/card-d-02.jpg",
-		"images/card-d-03.jpg",
-		"images/card-d-04.jpg",
-		"images/card-d-05.jpg",
-		"images/card-d-05.jpg",
-		"images/card-d-06.jpg",
-		"images/card-d-07.jpg",
-		"images/card-d-08.jpg",
-		"images/card-d-09.jpg",
-		"images/card-d-10.jpg",
-		"images/card-d-11.jpg",
-		"images/card-d-12.jpg",
-		"images/card-d-13.jpg",
-		"images/card-d-14.jpg"
-	];
-};
+function reset() {
+	deck = []; //se eliminan todos los elementos de la variable deck
+	for (let palo of palos) {
+		for (i = 1 ; i <= 14 ; i++) {
+			let numero = i.toString().padStart(2,"0");
+			deck.push("images/card-" + palo + "-" + numero + ".jpg");
+		}
+	}
+}
 
 function draw() {
 	const card = document.getElementById("cardOnTable");
@@ -76,11 +24,16 @@ function draw() {
 }
 
 function shuffle() {
-	reset();
 	deck.sort(function(){return 0.5 - Math.random()});
 	const card = document.getElementById("cardOnTable");
 	card.src = "images/card-0.jpg";
-};
+}
+
+drawBtn.addEventListener("click", draw);
+shuffleResetBtn.addEventListener("click", function() {
+	reset();
+	shuffle();
+})
 
 reset();
 shuffle();
