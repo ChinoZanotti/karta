468905bfc6e723851a2ec548eedd7e7c91baf3b8
ChinoZanotti <zanotti@arsis.com.py>
2025-07-18 18:08:00 -0300

    arrow
    cambie las funciones usando arrow y otros pequeños cambios

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
+const palos = ["a", "b", "c", "d"]; //se definen los palos o grupos que hay dentro del deck
 let deck = []; //se crea una variable "deck" vacía
-let palos = ["a", "b", "c", "d"]; //se definen los palos o grupos que hay dentro del deck
-let drawBtn = document.getElementById("drawBtn");
-let shuffleResetBtn = document.getElementById("shuffleResetBtn");
 
-function reset() {
+const drawBtn = document.getElementById("drawBtn");
+const shuffleResetBtn = document.getElementById("shuffleResetBtn");
+
+const reset = () => {
 	deck = []; //se eliminan todos los elementos de la variable deck
-	for (let palo of palos) {
-		for (i = 1 ; i <= 14 ; i++) {
-			let numero = i.toString().padStart(2,"0");
-			deck.push("images/card-" + palo + "-" + numero + ".jpg");
+	for (const palo of palos) {
+		for (let i = 1 ; i <= 14 ; i++) {
+			const numero = i.toString().padStart(2,"0");
+			deck.push(`images/card-${palo}-${numero}.jpg`);
 		}
 	}
-}
+};
 
-function draw() {
+const draw = () => {
 	const card = document.getElementById("cardOnTable");
 	if (deck.length > 0) {
 		card.src = deck.pop(); // suponiendo que el deck guarda solo las rutas
@@ -21,19 +22,20 @@ function draw() {
 	    card.src = "images/card-empty.jpg";
 	    card.classList.add("empty");
 	}
-}
+};
 
-function shuffle() {
-	deck.sort(function(){return 0.5 - Math.random()});
+const shuffle = () => {
+	deck.sort(() => 0.5 - Math.random());
 	const card = document.getElementById("cardOnTable");
 	card.src = "images/card-0.jpg";
-}
+};
 
-drawBtn.addEventListener("click", draw);
-shuffleResetBtn.addEventListener("click", function() {
+drawBtn.addEventListener("click", draw);//para robar una carta del deck
+
+shuffleResetBtn.addEventListener("click", () => {
 	reset();
 	shuffle();
-})
+});//para resetear y mezclar el deck
 
 reset();
 shuffle();
